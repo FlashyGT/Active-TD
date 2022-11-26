52b85d672c1f85fa2de4547e41dc882be6fd2c10
George Toliasvili <georgs.toliasvili@gmail.com>
2022-11-26 19:46:29 +0200

    Implemented a new and simpler combat system, refactored a bit

--- Assets/UnitCombat.cs
+++ Assets/UnitCombat.cs
@@ -1,10 +1,24 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class WeaponRange : MonoBehaviour
+public class UnitCombat : MonoBehaviour
 {
+    [SerializeField] private WeaponSO weaponSo;
+
     private Animator _animator;
     private List<Unit> _targets = new();
+
+    public void DealDamageToTargets()
+    {
+        // ToList() copies the existing list, so when damage gets dealt and a unit dies
+        // we don't modify the actual _targets list, but instead a copy of it.
+        // If we modify _targets directly we receive: InvalidOperationException
+        foreach (Unit unit in _targets.ToList())
+        {
+            GameManager.Instance.DamageUnit(unit, weaponSo.damage);
+        }
+    }
 
     private void Start()
     {
--- Assets/Utility/Unit.cs
+++ Assets/Utility/Unit.cs
@@ -4,41 +4,43 @@ using UnityEngine.Events;
 
 public class Unit : MonoBehaviour
 {
+    public UnitHealth UnitHealth { get; private set; }
+    public Animator Animator { get; private set; }
+    public Rigidbody Rigidbody { get; private set; }
+
     // Used for outside scripts
     public delegate void OnUnitDead(Unit unit);
 
+    public event OnUnitDead OnUnitDeath;
+
     public delegate void OnDamageTaken();
+
+    public OnDamageTaken OnDamageTake;
+
+    [SerializeField] private UnitCombat unitCombat;
 
     // Used for this specific unit to manage components
     [SerializeField] private UnityEvent onUnitDeath;
 
     [SerializeField] private UnitSO unitSo;
-    public UnitHealth UnitHealth { get; private set; }
-    public Animator Animator { get; private set; }
-    public Rigidbody Rigidbody { get; private set; }
-
-    private void Awake()
-    {
-        UnitHealth = new UnitHealth(unitSo.health, unitSo.health);
-        Rigidbody = GetComponent<Rigidbody>();
-        Animator = GetComponent<Animator>();
-    }
-
-    protected virtual void OnTriggerEnter(Collider collider)
-    {
-    }
-
-    protected virtual void OnTriggerExit(Collider collider)
-    {
-    }
-
-    public event OnUnitDead OnUnitDeath;
-    public OnDamageTaken OnDamageTake;
 
     public virtual void OnDeath()
     {
         Animator.SetTrigger(Constants.AnimDeathParam);
         onUnitDeath.Invoke();
         OnUnitDeath?.Invoke(this);
+    }
+
+    // Used for an event by the attacking animation
+    protected void DealDamage()
+    {
+        unitCombat.DealDamageToTargets();
+    }
+
+    private void Awake()
+    {
+        UnitHealth = new UnitHealth(unitSo.health, unitSo.health);
+        Rigidbody = GetComponent<Rigidbody>();
+        Animator = GetComponent<Animator>();
     }
 }
